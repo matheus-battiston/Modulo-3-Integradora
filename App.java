9ba8f7aee32c05974ba22e59c6b05c92a0658d1f
Matheus <matheus_battiston@hotmail.com>
2021-09-07 10:08:22 -0300

    Calculo Frequencia e arquivo texto basico para teste

--- App.java
+++ App.java
@@ -1,19 +1,55 @@
 import java.util.Hashtable;
+import java.io.*;
+import java.util.ArrayList;
+
 
 public class App {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		//FreqTable table = new FreqTable(3);
 		
 		Hashtable<Character,Double> table = new Hashtable<Character,Double>();
 		
-		table.put('a', 45.0);
-		table.put('b', 13.0);
-		table.put('c', 12.0);
-		table.put('d', 16.0);
-		table.put('e', 9.0);
-		table.put('f', 5.0); 
+		File texto = new File("texto.txt");
+		String linha;
+		int contador = 0;
+		Character aux;
+		double result;
+
+		ArrayList<Character> contado = new ArrayList();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(texto));
+			while ((linha = br.readLine()) != null){
+				char[] letras = linha.toCharArray();
+				for (int i = 0; i < linha.length(); i++){
+					for (int j = i; j < linha.length(); j++){
+						if (letras[i] == letras[j]){
+							if (contado.contains(letras[i])){
+								contador = contador + 1;
+							}else {
+								contado.add(letras[i]);
+								contador = contador + 1;
+							}
+						}
+					}
+
+					if (table.containsKey(letras[i])== false){
+						aux = letras[i];
+						result = contador;
+						table.put(aux,result);
+					}
+					contador = 0;
+				}
+
+				}
+		}
+		catch(IOException e) {
+				e.printStackTrace();
+
+		}
+
 		
 		HuffmanEncoding hf = new HuffmanEncoding(table);
 		
