80a7da848a7490b3d6147fd999a76e5f2893b8fc
Matheus <matheus_battiston@hotmail.com>
2021-09-17 00:29:45 -0300

    Teste funcionando

--- modulo3/src/test/java/com/integradora/AppTest.java
+++ modulo3/src/test/java/com/integradora/AppTest.java
@@ -3,6 +3,14 @@ package com.integradora;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import java.io.File;
 import java.util.Hashtable;
@@ -11,16 +19,27 @@ import java.util.Hashtable;
  * Unit test for simple App.
  */
 class AppTest {
-    @Test
-    void testApp() {
+    @ParameterizedTest
+    @CsvSource ({"3.0,2.0,1.0,texto.txt", "14.0,1.0,1.0,texto2.txt","1.0,1.0,1.0,texto3.txt", "1.0,0.0,0.0,texto4.txt"})
+    
+    void testApp(Double arg1, Double arg2, Double arg3, String texto) {
 
-        File file = new File(getClass().getResource("texto.txt").getFile());        	
+        File file = new File(getClass().getResource(texto).getFile());        	
 
         Hashtable<Character,Double> tabela = new Hashtable<Character,Double>();
 		
-		tabela.put('a', 3.0);
-		tabela.put('b', 2.0);
-		tabela.put('c', 1.0);
+        if (arg1 > 0.0){
+            tabela.put('a', arg1);
+
+        }
+        if(arg2 > 0.0){
+            tabela.put('b', arg2);
+        }
+		
+        if(arg3>0.0){
+            tabela.put('c', arg3);
+
+        }
 
 		        
         assertEquals(tabela, App.calcula_frequencia(file));
